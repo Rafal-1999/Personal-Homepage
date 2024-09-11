ab6aadc3e37d2d9fb9bf60d0534fbf0700cae302
Rafal Chudy <rafalchu5@gmail.com>
2024-09-11 22:43:45 +0200

    Skills section created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,21 @@
 import Container from "../components/Container";
 import BusinessCard from "../components/BusinessCard";
+import Skills from "../components/Skills";
+import { skills } from "../data/skills";
+import toolsIcon from "../assets/icons/decorative/tools.png";
 
 const Home = () => (
     <Container>
         <header>
             <BusinessCard />
         </header>
+        <main>
+            <Skills
+                title="My skillset includes"
+                decorativeIcon={toolsIcon}
+                arrayItems={skills}
+            />
+        </main>
     </Container>
 );
 
