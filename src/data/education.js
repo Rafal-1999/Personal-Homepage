a915ed8a1c670e67d8d40315e9c4c049df707541
Rafal Chudy <rafalchu5@gmail.com>
2024-09-11 21:13:14 +0200

    Data files added

---
+++ src/data/education.js
@@ -0,0 +1,8 @@
+export const education = [
+    "React Context",
+    "Node.js",
+    "Cypress",
+    "Unit testing",
+    "TypeScript",
+    "JS classes"
+];
---
+++ src/data/socialMedia.js
@@ -0,0 +1,22 @@
+export const socialMedia = [
+    {
+        name: "GitHub",
+        icon: "ikonka_1",
+        link: "https://github.com/Rafal-1999"
+    },
+    {
+        name: "Facebook",
+        icon: "ikonka_2",
+        link: "https://www.facebook.com"
+    },
+    {
+        name: "Linkedin",
+        icon: "ikonka_3",
+        link: "https://pl.linkedin.com"
+    },
+    {
+        name: "Instagram",
+        icon: "ikonka_4",
+        link: "https://www.instagram.com"
+    }
+];
